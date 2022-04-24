0c921eda7aed8f960540ff976db28befcd681be6
tomryb <1rybak.tomasz@gmail.com>
2022-04-24 17:32:25 +0200

    test

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = require("./server/server");
+const PORT = process.env.PORT || 5000;
 
-app.listen(4001, () => {
+// app.listen(4001, () => {
+//   console.log("Listening on port 4001");
+// });
+
+app.listen(PORT, () => {
   console.log("Listening on port 4001");
 });
